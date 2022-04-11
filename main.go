aa1494aa092d69095f8b059594f7103135a4a3ff
John Jacob <john.jacob@nomihealth.com>
2022-04-11 17:25:32 -0500

    fix write file handling

--- main.go
+++ main.go
@@ -9,45 +9,55 @@ import (
 	"text_mangler/manglers"
 )
 
-
-func main() {
-
+func getFlags() (inFilename, outFilename, manglerFlag string) {
 	inPtr := flag.String("infile", "", "file to read from")
 	outPtr := flag.String("outfile", "", "file to write to")
 	manglerPtr := flag.String("mangler", "", "which text transformer to use")
 
 	flag.Parse()
+	return *inPtr, *outPtr, *manglerPtr
+}
 
+func getFileHandles(inFilename, outFilename string) (*os.File, *os.File, error) {
 	var in, out *os.File
 	var err error
-	var mangler func(*os.File) []byte
-	if *inPtr == "" {
+	if inFilename == "" {
 		in = os.Stdin
 	} else {
-		pth := path.Clean(*inPtr)
+		pth := path.Clean(inFilename)
 		in, err = os.Open(pth)
 		if err != nil {
-			log.Fatal(err)
+			return nil, nil, err
 		}
 	}
 
-	fmt.Println("bloop")
-	if *outPtr == "" {
+	if outFilename == "" {
 		out = os.Stdout
 	} else {
-		pth := path.Clean(*outPtr)
-		out, err = os.Open(pth)
+		pth := path.Clean(outFilename)
+		out, err = os.OpenFile(pth, os.O_WRONLY, os.ModeAppend)
 		if err != nil {
-			log.Fatal(err)
+			return nil, nil, err
 		}
 	}
+	return in, out, nil
+}
 
+func main() {
+
+	inFilename, outFilename, manglerFlag := getFlags()
+
+	in, out, err := getFileHandles(inFilename, outFilename)
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer in.Close()
 	defer out.Close()
 
-	mangler, ok := manglers.Registry[*manglerPtr]
+
+	mangler, ok := manglers.Registry[manglerFlag]
 	if !ok {
-		err = fmt.Errorf("mangler with name %s not found in registry", *manglerPtr)
+		err = fmt.Errorf("mangler with name %s not found in registry", manglerFlag)
 		log.Fatal(err)
 	}
 
